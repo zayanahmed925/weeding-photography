395326c5bf2ec1f7f1607c01b13af21878a2dfb0
zayanahmed925 <rjraju925@gmail.com>
2022-04-18 14:41:56 +0600

    About page added

--- src/Page/Home/Home/About/About.js
+++ src/Page/Home/Home/About/About.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import './About.css';
 const About = () => {
     return (
-        <div>
-            <h2>This is About</h2>
+        <div className='container about-container row'>
+            <div className='col-sm-12 col-md-12 about-info'>
+                <h2 className='title'>Hi!! I am <span className='name'>Zehadul Islam</span></h2>
+                <p>I want to become a app developer.Currently I'm working on my react skills. I have worked with HTML, CSS and javascript for about 3 month. Now I want to work on react more as there are better growth opportunities.and also Planning on doing a certification in MERN stack so I can get into full stack development which I believe is going to be a great career choice for the future.I used to work from 8am-6pm, and then start learning to code from 8pm to 1am/2am for a few months. That was very challenging for me, but I needed to be highly focused and it worked out well because of that discipline.</p>
+            </div>
+            <div className='col-md-12'>
+
+            </div>
         </div>
     );
 };
